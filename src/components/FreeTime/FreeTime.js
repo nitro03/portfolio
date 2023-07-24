d705277192df58a78dc2e5e4e9a9a315349f1dd1
Krzysiek <nitro_gamer@op.pl>
2023-07-24 18:51:10 +0200

    Free Time section added - added content for free time - added images

--- src/components/FreeTime/FreeTime.js
+++ src/components/FreeTime/FreeTime.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {Container} from 'react-bootstrap';
+import translator from '../../i18n/translator';
+import hobbiesData from './HobbiesData';
+import Panel from '../Panel/Panel';
+
+import {LangContext} from '../../contexts/LangContext';
 
 function FreeTime() {
+
+    const {lang} = useContext(LangContext);
+    const renderFreeTimePanels = () => {
+        if (Array.isArray(hobbiesData)) {
+            const panelsList = hobbiesData.map((hobby) => {
+                const {imgSrc, descriptionLabel, title} = hobby;
+                return <Panel imgSrc={imgSrc}
+                              title={translator(title, lang)}
+                              description={translator(descriptionLabel, lang)}/>
+            });
+            return panelsList;
+        }
+        console.warn('Hobbies are empty');
+        return <div>No Data</div>;
+    }
+
     return (
-        <div className="freetime">free time</div>
+        <Container>
+            {renderFreeTimePanels()}
+        </Container>
     );
 }
 
---
+++ src/components/FreeTime/HobbiesData.js
@@ -0,0 +1,14 @@
+import dyiImg from '../FreeTime/img/dyi.jpg';
+import f14Img from '../FreeTime/img/f-14-grummann.jpg';
+import horusImg from '../FreeTime/img/horus.jpg';
+import psLogo from '../FreeTime/img/ps_logo.png';
+import soccerPlayerImg from '../FreeTime/img/soccer-player.jpg';
+
+const hobbiesData = [];
+
+hobbiesData.push({imgSrc: psLogo, title: 'gaming_title', descriptionLabel: 'gaming_desc'});
+hobbiesData.push({imgSrc: soccerPlayerImg, title: 'sport_title', descriptionLabel: 'sport_desc'});
+hobbiesData.push({imgSrc: horusImg, title: 'horus_title', descriptionLabel: 'horus_desc'});
+hobbiesData.push({imgSrc: f14Img, title: 'modelling_title', descriptionLabel: 'modelling_desc'});
+hobbiesData.push({imgSrc: dyiImg, title: 'dyi_title', descriptionLabel: 'dyi_desc'});
+export default hobbiesData;
