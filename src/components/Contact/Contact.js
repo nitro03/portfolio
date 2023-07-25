bab95fdbd54bed956e2fbf1f9e5c42f818cdd8f8
Krzysiek <nitro_gamer@op.pl>
2023-07-25 17:35:37 +0200

    Contact form - added form validation - added modal with confirmation

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,5 @@
 import React, {useContext, useState} from 'react';
-import {Button, Col, Container, Form, Row} from 'react-bootstrap';
+import {Button, Col, Container, Form, Row, Modal} from 'react-bootstrap';
 import translator from '../../i18n/translator';
 import {LangContext} from '../../contexts/LangContext';
 import EmailIcon from '@mui/icons-material/Email';
@@ -11,32 +11,35 @@ function Contact() {
     const INPUT_EMAIL_ID = 'formControlEmail';
     const INPUT_NAME_ID = 'formControlName';
     const INPUT_MSG_ID = 'formControlMsg';
-    const {lang} = useContext(LangContext);
-
-    const [formState, setFormState] = useState({
+    const INITIAL_STATE = {
         senderMail: '',
         senderName: '',
         msg: ''
-    });
-
-    const validateForm = () => {
-        //TODO
     };
 
-    const showConfirmation = function () {
-        //TODO: zrobić toast
-        alert(translator('contact_confirmation', lang));
-    };
+    const {lang} = useContext(LangContext);
+
+    const [formState, setFormState] = useState(INITIAL_STATE);
+    const [validated, setValidated] = useState(false);
+    const [isModalShown, showModal] = useState(false);
+    const toggleModal = () => showModal(!isModalShown);
 
     const clearInputs = () => {
-        //TODO
+        setFormState(INITIAL_STATE);
+        setValidated(false);
     }
     const onSubmit = (e) => {
         e.preventDefault();
+
+        const form = e.currentTarget;
+        if (form.checkValidity() === false) {
+            setValidated(true);
+            return;
+        }
+
         const currentDate = new Date().toISOString();
 
         const {senderMail, senderName, msg} = formState;
-
         const message = {
             senderMail,
             senderName,
@@ -45,12 +48,34 @@ function Contact() {
         }
         //tu powinna byc akcja wyslania wiadomosci na maila
         console.log(message);
+        toggleModal();
+        clearInputs();
     }
     const renderInfo = () => {
         return (
             <p className="contact-info">
                 {translator('contact_info', lang)}
-            </p>);
+            </p>
+        );
+    };
+
+    const renderModal = () => {
+
+        return (
+            <Modal show={isModalShown} onHide={toggleModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{translator('message_sent', lang)}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>{translator('contact_confirmation', lang)}</Modal.Body>
+                <Modal.Footer>
+
+                    <Button variant="primary" onClick={toggleModal}>
+                        {translator('close', lang)}
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        );
+
     }
 
     const onInputChange = (e) => {
@@ -78,29 +103,31 @@ function Contact() {
                         msg: value
                     })
                     break;
+                default:
             }
+            setValidated(true);
             return;
         }
         console.error('Input Id is empty, state was\'nt set');
-        return;
     }
     const renderForm = () => {
+        const {senderMail, senderName, msg} = formState
         return (
-            <Form className="contact-form">
+            <Form noValidate validated={validated} onSubmit={onSubmit} className="contact-form">
                 <Form.Group className="mb-3" controlId={INPUT_EMAIL_ID}>
                     <Form.Label>{translator('email_label', lang)}</Form.Label>
-                    <Form.Control type="email" placeholder="name@example.com" onChange={onInputChange}/>
+                    <Form.Control type="email" required={true} placeholder="name@example.com" value={senderMail}
+                                  onChange={onInputChange}/>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId={INPUT_NAME_ID}>
                     <Form.Label>{translator('name_label', lang)}</Form.Label>
-                    <Form.Control onChange={onInputChange}/>
+                    <Form.Control value={senderName} required onChange={onInputChange}/>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId={INPUT_MSG_ID}>
                     <Form.Label>{translator('message_label', lang)}</Form.Label>
-                    <Form.Control as="textarea" rows={MSG_FIELD_ROWS} onChange={onInputChange}/>
+                    <Form.Control as="textarea" required rows={MSG_FIELD_ROWS} value={msg} onChange={onInputChange}/>
                 </Form.Group>
-                <Button className="contact-form__submit-btn" variant="primary" type="submit" size="lg"
-                        onClick={onSubmit}>
+                <Button className="contact-form__submit-btn" variant="primary" type="submit" size="lg">
                     <EmailIcon/>
                     <span className="contact-form__submit-btn__span">{translator('submit_btn_label', lang)}</span>
                 </Button>
@@ -112,6 +139,7 @@ function Contact() {
             <Row className="justify-content-md-center">
                 <Col lg="6">
                     {renderInfo()}
+                    {renderModal()}
                 </Col>
             </Row>
             <Row className="justify-content-md-center">
