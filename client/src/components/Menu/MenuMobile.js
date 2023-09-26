9c75b3eecdae6ee7b5925f31738bf9c0cf90a40f
Krzysiek <nitro_gamer@op.pl>
2023-09-26 20:32:40 +0200

    - fixed contact form focus - fixed mobile menu state

--- client/src/components/Menu/MenuMobile.js
+++ client/src/components/Menu/MenuMobile.js
@@ -7,13 +7,15 @@ import MenuHamburger from './MenuHamburger';
 import MenuItem from './MenuItem';
 
 import "./css/menu.scss";
+import {NavContext} from '../../contexts/NavContext';
 
 MenuMobile.propTypes = {};
 
 function MenuMobile() {
     const {lang, setLang} = useContext(LangContext);
+    const {link, setLink} = useContext(NavContext);
+
     const [isExpanded, setIsExpanded] = useState(false);
-    const [activeKey, setActiveKey] = useState(window?.location?.pathname)
 
     const renderButton = () => {
         return (
@@ -26,15 +28,15 @@ function MenuMobile() {
             setIsExpanded(false);
         }
         return (<div className={classes}>
-                <MenuItem url='/' label={translator("home", lang)} activeKey={activeKey} setActiveKey={setActiveKey} onClick={onClick}/>
-                <MenuItem url='/career' label={translator("Career", lang)} activeKey={activeKey}
-                          setActiveKey={setActiveKey} onClick={onClick}/>
-                <MenuItem url='/projects' label={translator("Projects", lang)} activeKey={activeKey}
-                          setActiveKey={setActiveKey} onClick={onClick}/>
-                <MenuItem url='/freetime' label={translator("Free_Time", lang)} activeKey={activeKey}
-                          setActiveKey={setActiveKey} onClick={onClick}/>
-                <MenuItem url='/contact' label={translator("Contact", lang)} activeKey={activeKey}
-                          setActiveKey={setActiveKey} onClick={onClick}/>
+                <MenuItem url='/' label={translator("home", lang)} activeKey={link} setActiveKey={setLink} onClick={onClick}/>
+                <MenuItem url='/career' label={translator("Career", lang)} activeKey={link}
+                          setActiveKey={setLink} onClick={onClick}/>
+                <MenuItem url='/projects' label={translator("Projects", lang)} activeKey={link}
+                          setActiveKey={setLink} onClick={onClick}/>
+                <MenuItem url='/freetime' label={translator("Free_Time", lang)} activeKey={link}
+                          setActiveKey={setLink} onClick={onClick}/>
+                <MenuItem url='/contact' label={translator("Contact", lang)} activeKey={link}
+                          setActiveKey={setLink} onClick={onClick}/>
             </div>
         );
     }
