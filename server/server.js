647f652b4bb5c048b06238d8c894d2da50081d70
krzysztof.bartosik <kbartosik90@gmail.com>
2024-01-22 20:47:22 +0100

    background for netlify

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const serverless = require('serverless-http');
+const path = require('path');
+const bodyParser = require('body-parser');
+const config = require('./config/config.json');
+const MailSender = require('./MailSender/MailSender');
+const app = express()
+
+const port = 1234
+const {transporterOptions} = config;
+const ms = MailSender.getInstance(transporterOptions);
+const router = express.Router();
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
+router.post('/rest/send-mail', (req, res) => {
+    const body = req.body;
+
+    const onError = (err) => {
+        res.status(500).json(err).send();
+    };
+    const onSuccess = (info) => {
+        res.status(200).json(info).send();
+    };
+
+    ms.send(body, onSuccess, onError);
+});
+
+app.use(express.static("../client/build"));
+app.use(bodyParser.json());
+app.use('/', router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
