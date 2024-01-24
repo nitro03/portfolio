54a9ef77cf2498e9978569bb2be2395b42141ad5
krzysztof.bartosik <kbartosik90@gmail.com>
2024-01-24 16:27:39 +0100

    - mail sending turned on

--- api/send-mail.js
+++ api/send-mail.js
@@ -4,15 +4,14 @@ const {transporterOptions} = config;
 const ms = MailSender.getInstance(transporterOptions);
 
 module.exports = (req, res) => {
-    res.status(200).json({ message: 'Hello from the serverless function!' });
-    // const body = req.body;
-    //
-    // const onError = (err) => {
-    //     res.status(500).json(err).send();
-    // };
-    // const onSuccess = (info) => {
-    //     res.status(200).json(info).send();
-    // };
-    //
-    // ms.send(body, onSuccess, onError);
+    const body = req.body;
+
+    const onError = (err) => {
+        res.status(500).json(err).send();
+    };
+    const onSuccess = (info) => {
+        res.status(200).json(info).send();
+    };
+
+    ms.send(body, onSuccess, onError);
 };
